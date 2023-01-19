90161cf8b6a9df160a6cff4ec21f58c4367241e4
Arslan591 <68608524+Arslan591@users.noreply.github.com>
2023-01-19 17:02:40 +0500

    Revert "make some changes"
    This reverts commit b290b730d5566f5cd7723fe7df36317158383f1d.

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,39 +1,30 @@
 const express = require("express");
-const Data = require("../models/product");
+const productServices = require("../services/productServices");
 
 const getAllProducts = async (req, res) => {
-  const AllProducts = await Data.findAll();
+  const AllProducts = await productServices.getAllProducts(req);
   res.send(AllProducts);
 };
 
 const getProductById = async (req, res) => {
-  const _id = req.params.id;
-  const specificProduct = await Data.findByPk(_id);
-  res.send(specificProduct);
+  const specificProducts = await productServices.getProductById(req);
+  res.send(specificProducts);
 };
 
-// const updateProductById = async (req, res) => {
-//   const _id = req.params.id;
-//   const updateProduct = await Data.findByIdAndUpdate(_id, req.body);
-//   // res.send(allProducts);
-//   console.log(updateProduct, "Hello");
-// };
+const updateProductById = async (req, res) => {
+  const updateProduct = await productServices.updateProduct(req);
+  res.send(updateProduct);
+};
 
-// const deleteProductById = async (req, res) => {
-//   const deleteProduct = await Data.findByIdAndDelete(req.params.id);
-//   // res.send(allProducts);
-//   console.log(deleteProduct, "Hello");
-// };
+const deleteProductById = async (req, res) => {
+  const deleteProduct = await productServices.deleteProductById(req);
+
+  res.send(deleteProduct);
+};
 
 const createProduct = async (req, res) => {
-  try {
-    const insertNewProduct = await Data(req.body);
-    console.log(req.body);
-    const productAdded = insertNewProduct.save();
-    res.send(productAdded);
-  } catch (e) {
-    res.send(e);
-  }
+  const productinsert = await productServices.createProduct(req);
+  res.send(productinsert);
 };
 
 module.exports = {
